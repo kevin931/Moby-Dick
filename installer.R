07d03752351dbe32d8378e22830c2b14a1b852e7
KAIWEN WANG <kaiwenwang@KAIWENs-MacBook-Air.local>
2018-11-14 21:29:30 -0600

    updated installer

--- installer.R
+++ installer.R
@@ -1,3 +1,8 @@
 #### 
 #Installer
 ####
+
+packages<-c("tidyverse", "data.table", "openNLP", "tidytext", "plotly",
+            "quanteda", "shiny", "gutenbergr", "parallel", "tm")
+
+install.packages(packages)
